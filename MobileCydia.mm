252f30db1bdb676fa6b5bdc3a664541582c78b3a
Jay Freeman (saurik) <saurik@saurik.com>
2018-08-13 16:07:25 -0700

    Remove ultrasn0w repository (as they disappeared).

--- MobileCydia.mm
+++ MobileCydia.mm
@@ -9414,7 +9414,6 @@ int main(int argc, char *argv[]) {
         CydiaAddSource(@"http://apt.thebigboss.org/repofiles/cydia/", @"stable", [NSMutableArray arrayWithObject:@"main"]);
         CydiaAddSource(@"http://apt.modmyi.com/", @"stable", [NSMutableArray arrayWithObject:@"main"]);
         CydiaAddSource(@"http://cydia.zodttd.com/repo/cydia/", @"stable", [NSMutableArray arrayWithObject:@"main"]);
-        CydiaAddSource(@"http://repo666.ultrasn0w.com/", @"./");
 
         Version_ = [NSNumber numberWithUnsignedInt:1];
 
